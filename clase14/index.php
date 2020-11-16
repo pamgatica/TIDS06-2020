90c564b29143cd291beccf32b624b086d5d507b8
Pame Gatica <pam.gatica@profesor.duoc.cl>
2020-11-16 12:04:36 -0300

    Clase 14 - Orientación a objetos y paso de parámetros
    Orientación a objetos, paso de parámetros y uso de componentes bootstrap

---
+++ clase14/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tareas Pendientes</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body style="padding:20px"> 
+    <h1>Tareas Pendientes</h1>
+    <a href="nuevatarea.php">Nueva Tarea</a>
+    <hr>
+    <strong>Desarrollo de Aplicaciones Web</strong><br>
+    INACAP Valparaíso <br>
+    Primavera 2020
+</body>
+</html>
